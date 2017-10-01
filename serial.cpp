5e9389c0fed87d63d525b3c4928bb86b0c00f1d6
Pietro Ferretti <pietro.ferretti1@gmail.com>
2017-10-01 19:40:19 +0200

    Moved serial algorithm into separate files

---
+++ serial.cpp
@@ -0,0 +1,10 @@
+#include "serial.hpp"
+#include <vector>
+
+unsigned long long int serialSum(std::vector<unsigned long long int> addends) {
+	unsigned long long int sum = 0;
+	for(unsigned long long int addend: addends){
+		sum += addend;
+	}
+	return sum;
+}
---
+++ serial.hpp
@@ -0,0 +1,8 @@
+#ifndef serial_h
+#define serial_h
+
+#include <vector>
+
+unsigned long long int serialSum(std::vector<unsigned long long int> addends);
+
+#endif
