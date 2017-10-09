dbd05253204ae5966e6a35db95e4587bd2526c8b
valeionny <10452850@polimi.it>
2017-10-09 14:10:32 +0200

    parallel logarithmic sum with openmp

--- src/parallel.cpp
+++ src/parallel.cpp
@@ -2,35 +2,30 @@
 #include <cmath>
 #include <vector>
 #include <iostream>
+#include <omp.h>
 
 unsigned long long int parallelSum(std::vector<unsigned long long int> addends) {
 	
 	unsigned long long int sum = 0;
 	int n= addends.size();
 	std::cout << n << "\n";
-	
+	int j=0;
+	//all the logarithmic sum
 	while(n!=1){
-	
-		int i=0;
+	 
+		unsigned int i=0;
+		//eaxh step of the sum put the new numbers in esponential position to avoid thread overlapping in writing variables 
+		#pragma omp parallel for 
 		for(i=0; i<n/2; i++){
-		
-			addends[i]=addends[2*i]+addends[2*i+1];	
-			std::cout << "ciclo \n";
-		}
-		
-		if(n%2!=0){
 			
-			addends[i]= addends[2*i];
+			addends[(1<<j+1)*i]=addends[(1<<j+1)*i]+addends[(1<<j+1)*i+(1<<j)];	
 		}
 		
 		n=(n+1)/2;
-
-		
-		for(unsigned long long int addend: addends){
-			std::cout << addend <<" ";
-		}
-		std::cout <<"\n";
+		j++;
+	
 	}
+	//the result is finally put in the first element of the vector		
 	sum=addends[0];
 	return sum;
 }
